d2045267b7ae5ddcac6b6ef5f9345da967860d4a
Younis Rahman <younisrahman007@gmail.com>
2021-12-07 23:55:00 +0600

    SieveOfEratosthenes.py

--- SieveOfEratosthenes.py
+++ SieveOfEratosthenes.py
@@ -5,6 +5,8 @@ for i in range(2, int(n**0.5)+1):
     if primes[i]:
         for j in range(i*i, n+1, i):
             primes[j] = False
+
+
 for i in range(len(primes)):
     if primes[i]:
         print(i, end=" ")
